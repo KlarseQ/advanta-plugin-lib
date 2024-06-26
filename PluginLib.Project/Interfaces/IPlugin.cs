e1fe5b1d5d6e306f03f40439357f6cf09f86f2c7
KlarseQ <klarse4ever@gmail.com>
2024-06-26 13:02:39 +0300

    feat: init and add interfaces

---
+++ PluginLib.Project/Interfaces/IPlugin.cs
@@ -0,0 +1,32 @@
+﻿namespace ds.test.impl;
+
+/// <summary>
+/// Интерфейс для плагинов.
+/// </summary>
+public interface IPlugin
+{
+    /// <summary>
+    /// Имя.
+    /// </summary>
+    public string PluginName { get; }
+    
+    /// <summary>
+    /// Версия.
+    /// </summary>
+    public string Version { get; }
+    
+    /// <summary>
+    /// Изображение.
+    /// </summary>
+    public System.Drawing.Image Image { get; }
+    
+    /// <summary>
+    /// Описание.
+    /// </summary>
+    public string Description { get; }
+
+    /// <summary>
+    /// Выполнение.
+    /// </summary>
+    public int Run(int input1, int input2);
+}
---
+++ PluginLib.Project/Interfaces/IPluginFactory.cs
@@ -0,0 +1,24 @@
+﻿namespace ds.test.impl;
+
+/// <summary>
+/// Интерфейс для фабрики плагинов.
+/// </summary>
+public interface IPluginFactory
+{
+    /// <summary>
+    /// Количество плагинов.
+    /// </summary>
+    public int PluginsCount { get; }
+
+    /// <summary>
+    /// Имена всех плагинов.
+    /// </summary>
+    public string[] GetPluginNames { get; }
+
+    /// <summary>
+    /// Получить плагин по имени.
+    /// </summary>
+    /// <param name="pluginName">Имя плагина</param>
+    /// <returns>Плагин</returns>
+    public IPlugin GetPlugin(string pluginName);
+}
